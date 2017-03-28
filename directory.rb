42cbd38003e46a9909a25d835887ad1c2732ebce
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-03-28 22:10:31 +0100

    Using arrays

--- directory.rb
+++ directory.rb
@@ -1,18 +1,26 @@
 student_count = 11
-# This prints a list of the students
-puts "The students of Villians Academy"
+# let's put all the student into an array
+students = [
+  "Dr. Hannibal Lector",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# and then print them
+puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lector"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# We are printing the total numbers of students
-print "Overall, we have #{student_count} great students"
-# Print does not add a new line character
+students.each do |student|
+  puts student
+end
+# Operation on every value of an array using iteration
+# name will be assigned to the value student
+# now we print the total
+print "Overall, we have #{student.count} great students"
+# did it print?
