c1c9b7178be5aca9a0e84b59d55741072b478a97
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-03-29 20:34:51 +0100

    Asking for user input

--- directory.rb
+++ directory.rb
@@ -1,19 +1,28 @@
-student_count = 11
-# let's put all the student into an array
-students = [
-  {name: "Dr. Hannibal Lector", cohort: :november},
-  {name: "Darth Vader",cohort:  :november},
-  {name: "Nurse Ratched",cohort:  :november},
-  {name: "Michael Corleone",cohort:  :november},
-  {name: "Alex DeLarge",cohort:  :november},
-  {name: "The Wicked Witch of the West",cohort:  :november},
-  {name: "Terminator",cohort:  :november},
-  {name: "Freddy Krueger",cohort:  :november},
-  {name: "The Joker",cohort:  :november},
-  {name: "Joffrey Baratheon",cohort:  :november},
-  {name: "Norman Bates",cohort:  :november}
-]
-# key and value
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students =[]
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+# if the name the user entered is not empty
+# the block will be executed at least once
+# if not it will be skipped altogether and jump to line 17
+# << shovel operator (used to put things in array)
+# cohort value is hardcoded
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -28,11 +37,8 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-# nothing happens until we call the methods
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
-# the student variable is now passed to the
-# method as an argument (name)
-# this is because methods don't have access to
-# local variables defined outside them
