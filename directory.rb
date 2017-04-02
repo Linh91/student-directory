b4b645688f200687ba6ecd6136f19929c8801ccf
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-04-02 19:07:50 +0100

    Saving data to file

--- directory.rb
+++ directory.rb
@@ -3,8 +3,6 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  # students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -28,6 +26,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we will add more items
 end
 
@@ -43,6 +42,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -64,5 +65,18 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+# If we want to write a file, we need to "open" file first
 
 interactive_menu
